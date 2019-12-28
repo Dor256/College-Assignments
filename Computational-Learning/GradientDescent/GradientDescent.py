5bafb0a3989400261b4755c31d75ec529c750ab4
Dor <dorjp6@gmail.com>
2019-12-28 16:38:39 +0200

    extract metric functions

--- Computational-Learning/GradientDescent/GradientDescent.py
+++ Computational-Learning/GradientDescent/GradientDescent.py
@@ -10,11 +10,5 @@ def gradient_descent(f, gradient, w0, learning_rate, obj_tol, param_tol, max_ite
         w_next = w_curr - learning_rate * gradient(w_curr)
         iter_count += 1
         path = path[:] + [w_next]
-        # print(title)
-        # print("Iteration:", iter_count)
-        # print("Location is:", w_curr)
-        # print("Objective Value:", f(w_curr))
-        # print("Step:", np.linalg.norm(np.abs(w_next - w_curr)))
-        # print("Change in obj value:", np.abs(f(w_next)[0] - f(w_curr)[0]), "\n")
-    # print("Reached Convergence") if iter_count < max_iter else print("Failed to Converge")
+    print("Reached Convergence") if iter_count < max_iter else print("Failed to Converge")
     return path, w_next
