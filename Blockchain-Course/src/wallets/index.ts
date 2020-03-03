d8de3db69040d3af87b5b0838fcdea58e8deed9d
Dor <dorjp6@gmail.com>
2020-03-03 22:56:43 +0200

    Add web technologies course folder

--- Blockchain-Course/src/wallets/index.ts
+++
@@ -1,6 +0,0 @@
-export const dorPublicKey = '0296ac0d85ea09045c3fb3b1edb8d1709f7900e42be023ddc44517fc6cec0e7691';
-export const dorPrivateKey = '21654677e580003589096c21653ffe10435c0b11c7ba52b3c6eebae2ad6292cb';
-export const shaharPublicKey = '036a2503c4df0d535fac1827bf3983c56867b06c4bc9f2903e75b04156074d3723';
-export const shaharPrivateKey = '6e168491659629111216d9883e6df81b4efd371e99fc3ef46412facb74ca679b';
-const minerPublicKey = '02261b21985a049b794596dca964934aa401c3b1234bf755ce9865616d12ece8af';
-export const minerPrivateKey = '75a8a65714e0d9688bd7697238789ad53cd1f7f87bed70eb99e03ac3cd0e12a7';
