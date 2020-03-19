0f787e84793c96e09e43fe27d995613924b5ab11
Dor <dorjp6@gmail.com>
2020-03-19 21:59:17 +0200

    finish working on Web first-site project

--- Web/first-site/src/index.ts
+++ Web/first-site/src/index.ts
@@ -12,6 +12,22 @@ app.get('/nyc', (req, res) => {
     res.sendFile(`${publicPath}/nyc.html`);
 });
 
+app.get('/angelus', (req, res) => {
+    res.sendFile(`${publicPath}/angelus.html`);
+});
+
+app.get('/doom', (req, res) => {
+    res.sendFile(`${publicPath}/doom.html`);
+});
+
+app.get('/milford', (req, res) => {
+    res.sendFile(`${publicPath}/milford.html`);
+});
+
+app.get('/ir', (req, res) => {
+    res.sendFile(`${publicPath}/glendalough.html`);
+});
+
 app.get('/', (req, res) => {
     res.sendFile(`${publicPath}/index.html`);
 });
